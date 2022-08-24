a709b12e5214f02da0fb29faf2aa9e3c63805a8f
unknown <gkirchhoff32@gmail.com>
2022-08-23 22:30:27 -0600

    fixed 3d plot over time visualization plot

--- fipex/scripts/AD2StabilityAnalysis.m
+++ fipex/scripts/AD2StabilityAnalysis.m
@@ -1,12 +1,13 @@
 close all; clear all; clearvar; clc; format longEng;
 
+%%
+
 % P = append(pwd, '\..\data\2022_07_26_TIA_Repeat_Test\20220726_M0004_No_Cap_25ms_328kHz\');
-P = 'C:\Users\Grant\OneDrive - UCB-O365\Grad Projects\FIPEX Tests\fipex\data\2022_08_23_0deg_Incident\Zero_Degree_Incident\';
+P = 'C:\Users\jason\OneDrive - UCB-O365\Grad Projects\FIPEX Tests\fipex\data\2022_08_23_00deg_Incident\Zero_Degree_Incident\';
 S = dir(fullfile(P,'*.csv')); 
 for j = 1:numel(S)
     S(j).data = readmatrix(append(P, S(j).name));
 end
-
 
 %%
 exclude_bnd = 2276;  % This index corresponds w/ the time preceding the ringing.
@@ -19,22 +20,22 @@ dt = difft(1);
 vThresh = 0.17;  % [V] Threshold voltage where lower voltages are zeroed
 tThresh = 0.4e-3;  % [s] Threshold time where subsequent lower voltages are zeroed
 
-I_s = zeros(numel(S), length(S(1).data(1:exclude_bnd, 2)));
+I_s = zeros(length(S(1).data(1:exclude_bnd, 2)), numel(S));
 riemann_sum = zeros(1, numel(S));
 figure(1)
 hold on
 for j = 1:numel(S)
-    voltage = S(j).data(1:exclude_bnd,2);
+    voltage = S(j).data(1:exclude_bnd, 2);
     voltage = voltage - bias;  % Remove bias
     voltage(voltage<vThresh & t>tThresh) = 0;  % Zero any value below threshold to mitigate effect of ringing on integral
     current = voltage / gain;
+    I_s(:, j) = current;
     riemann_sum(1, j) = sum(current) * dt;
-    plot(t*1e3, current*1e9, '.')
-%     plot(current*1e9, '.')
-
+    plot3(t*1e3, repmat(j, length(t), 1), current*1e9)
 end
-ylabel('Current [nA]')
+ylabel('Laser shot')
 xlabel('Time [ms]')
+zlabel('Current [nA]')
 title('Superposed Pulses')
 
 peakCurrent = zeros(numel(S), 1);
